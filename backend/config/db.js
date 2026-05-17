71915bfcd0cb8417f9e45d01803bea723f4c8f1c
Mohamed Arshad <mxshadx@gmail.com>
2026-05-17 17:19:50 +0530

    connected mongoDB

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () =>{
+    try{
+        const uri= process.env.MONGO_URI;
+        if(!uri) throw new Error("MONGO_URI IS NOT DEFINED");
+        const conn = await mongoose.connect(uri);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);        
+    } catch (err){
+        console.log("MongoDB connection error:" ,err.message);
+        process.exit(1);
+    }
+};
